d7b03e8463ef6106c812d136c296dcca1623ce05
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-05-03 14:12:22 -0400

    NameRenderer is not a stat !

---
+++ CheesecakeCardBuilder/Renderer/CardParts/NameRenderer.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheesecakeCardBuilder.Renderer.CardParts {
+    using System;
+    using System.Drawing;
+    using Config;
+    using Unit;
+
+    public class NameRenderer : CardPartRenderer {
+        private static readonly Rectangle DEFAULTPOSITION = new Rectangle(170, 360, 450, 200);
+        private ProjectConfig config;
+        private UnitCard card;
+        private Font font;
+        private Brush brush;
+        private StringFormat format;
+
+        public NameRenderer(ProjectConfig config, UnitCard card) {
+            this.config = config;
+            this.card = card;
+            this.font = new Font(config.nameFont, config.nameFontSize, FontStyle.Regular);
+            this.brush = FontService.getDefaultBrush();
+            this.format = new StringFormat();
+            this.format.Alignment = StringAlignment.Center;
+            this.format.LineAlignment = StringAlignment.Far;
+        }
+
+        public void draw(Graphics graphics) {
+            graphics.DrawString(card.name, font, brush, DEFAULTPOSITION, format);
+        }
+    }
+}
